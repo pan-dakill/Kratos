0f2ea0d120d5eb550189d2e982a185addec35e9e
Ignasi de Pouplana <ipouplana@cimne.upc.edu>
2021-03-30 10:35:48 +0200

    Debug...

--- applications/StructuralMechanicsApplication/custom_strategies/custom_schemes/explicit_cd_fic_scheme.hpp
+++ applications/StructuralMechanicsApplication/custom_strategies/custom_schemes/explicit_cd_fic_scheme.hpp
@@ -298,6 +298,8 @@ public:
         LocalSystemVectorType RHS_Contribution = LocalSystemVectorType(0);
         Element::EquationIdVectorType equation_id_vector_dummy; // Dummy
 
+        KRATOS_WATCH("CD-FIC: CalculateAndAddRHS")
+
         #pragma omp parallel for firstprivate(RHS_Contribution, equation_id_vector_dummy), schedule(guided,512)
         for (int i = 0; i < static_cast<int>(r_conditions.size()); ++i) {
             auto it_cond = r_conditions.begin() + i;
