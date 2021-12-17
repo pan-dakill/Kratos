7c6a8f4d1b7e95b948166f4cc566c251004c1e39
Franci <falessandro@cimne.upc.edu>
2021-12-17 10:54:22 +0100

    averaged slip

--- applications/PfemFluidDynamicsApplication/custom_constitutive/fluid_laws/frictional_viscoplastic_2D_law.cpp
+++ applications/PfemFluidDynamicsApplication/custom_constitutive/fluid_laws/frictional_viscoplastic_2D_law.cpp
@@ -66,32 +66,32 @@ namespace Kratos
         const double dynamic_viscosity = this->GetEffectiveDynamicViscosity(rValues);
         double friction_angle = r_properties[INTERNAL_FRICTION_ANGLE];
 
-        //////////////////////////////////////////////////////
-        // slip friction on boundaries
-        for (unsigned int i = 0; i < number_of_nodes; i++)
-        {
-            if (r_geometry[i].Is(RIGID))
-            {
-                friction_angle = 24;
-            }
-        }
-        //////////////////////////////////////////////////////
-
-        //////////////////////////////////////////////////////
-        // // average friction on boundaries
-        // friction_angle =0;
+        // //////////////////////////////////////////////////////
+        // // slip friction on boundaries
         // for (unsigned int i = 0; i < number_of_nodes; i++)
         // {
         //     if (r_geometry[i].Is(RIGID))
         //     {
-        //         friction_angle += 24 / 3.0;
-        //     }
-        //     else
-        //     {
-        //         friction_angle += r_properties[INTERNAL_FRICTION_ANGLE] / 3.0;
+        //         friction_angle = 24;
         //     }
         // }
-        //////////////////////////////////////////////////////
+        // //////////////////////////////////////////////////////
+
+        ////////////////////////////////////////////////////
+        // average friction on boundaries
+        friction_angle =0;
+        for (unsigned int i = 0; i < number_of_nodes; i++)
+        {
+            if (r_geometry[i].Is(RIGID))
+            {
+                friction_angle += 24 / 3.0;
+            }
+            else
+            {
+                friction_angle += r_properties[INTERNAL_FRICTION_ANGLE] / 3.0;
+            }
+        }
+        ////////////////////////////////////////////////////
 
         const double cohesion = r_properties[COHESION];
         const double adaptive_exponent = r_properties[ADAPTIVE_EXPONENT];
